14a15b2c3e47b6a3f7b145ec906c24aad0c3ae9f
KevinVHoutte <kevin.van.houtte1@gmail.be>
2021-04-08 12:53:15 +0200

    make public method private

--- src/main/java/com/kvh/users/adapters/in/web/UserController.java
+++ src/main/java/com/kvh/users/adapters/in/web/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping
-    public UserResource getUser(){
+    private UserResource getUser(){
         return new UserResource("Bob");
     }
 
